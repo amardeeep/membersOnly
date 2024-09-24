c055fb14a923f281875d5cefff4976c963fba0b2
Deetsy <amardeepdeepak2016@gmail.com>
2024-09-24 14:53:26 +0530

    add flex wherever necessary to home elements to create a layout but sizing was not handled

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+const assetPath = path.join(__dirname, "public");
+app.use(express.static(assetPath));
 //authenication middleware
 //create strategy
 passport.use(
